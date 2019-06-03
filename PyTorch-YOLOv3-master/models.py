6434bfbf796c41df5576e8e093dd15c4350333ca
yeqinghuang516 <47376258+yeqinghuang516@users.noreply.github.com>
2019-06-03 13:08:53 -0700

    Update models.py

--- PyTorch-YOLOv3-master/models.py
+++ PyTorch-YOLOv3-master/models.py
@@ -210,7 +210,7 @@ class YOLOLayer(nn.Module):
                 loss_conf_obj = self.mse_loss(pred_conf[obj_mask], tconf[obj_mask])
                 loss_conf_noobj = self.mse_loss(pred_conf[noobj_mask], tconf[noobj_mask])
                 loss_conf = self.obj_scale * loss_conf_obj + self.noobj_scale * loss_conf_noobj
-                loss_cls = self.bce_loss(pred_cls[obj_mask], tcls[obj_mask])
+                loss_cls = self.mse_loss(pred_cls[obj_mask], tcls[obj_mask])
               
             total_loss = loss_x + loss_y + loss_w + loss_h + loss_conf + loss_cls
 
