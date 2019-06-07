5b9d8b847ff0cad1bd84e9b94ff278eaa6f9c476
yeqinghuang516 <47376258+yeqinghuang516@users.noreply.github.com>
2019-06-06 22:05:15 -0700

    Update models.py

--- PyTorch-YOLOv3-master/models.py
+++ PyTorch-YOLOv3-master/models.py
@@ -193,15 +193,14 @@ class YOLOLayer(nn.Module):
             # Loss : Mask outputs to ignore non-existing objects (except with conf. loss)
             if loss_mode is "modified":
                 eps = 1e-18
-                print("pred_conf[obj_mask]: ",pred_conf[obj_mask])
                 loss_x = self.mse_loss(x[obj_mask], tx[obj_mask])
                 loss_y = self.mse_loss(y[obj_mask], ty[obj_mask])
                 loss_w = self.mse_loss(torch.sqrt(torch.clamp(w[obj_mask], eps)), torch.sqrt(torch.clamp(tw[obj_mask], eps)))
                 loss_h = self.mse_loss(torch.sqrt(torch.clamp(h[obj_mask], eps)), torch.sqrt(torch.clamp(th[obj_mask], eps)))
-                loss_conf_obj = self.mse_loss(pred_conf[obj_mask], tconf[obj_mask])
-                loss_conf_noobj = self.mse_loss(pred_conf[noobj_mask], tconf[noobj_mask])
+                loss_conf_obj = self.mse_loss(pred_conf[obj_mask] * iou_scores, tconf[obj_mask])
+                loss_conf_noobj = self.mse_loss(pred_conf[noobj_mask] * iou_scores, tconf[noobj_mask])
                 loss_conf = self.obj_scale * loss_conf_obj + self.noobj_scale * loss_conf_noobj
-                loss_cls = self.mse_loss(pred_cls[obj_mask], tcls[obj_mask])
+                loss_cls = self.mse_loss(pred_cls[obj_mask] * * iou_scores, tcls[obj_mask])
             elif loss_mode is "unmodified" :
                 loss_x = self.mse_loss(x[obj_mask], tx[obj_mask])
                 loss_y = self.mse_loss(y[obj_mask], ty[obj_mask])
